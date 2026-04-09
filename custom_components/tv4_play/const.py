cb99174089457887940c4b7d99d59919c6ab3141
pepzi <robert@pepzi.org>
2026-04-09 22:55:56 +0200

    Add configuration and service constants
    Added new configuration and service constants for the TV4 Play integration.

--- custom_components/tv4_play/const.py
+++ custom_components/tv4_play/const.py
@@ -1,4 +1,10 @@
+"""Constants for the TV4 Play integration."""
+
 DOMAIN = "tv4_play"
 
-CONF_ENTRY_NAME = "entry_name"
-CONF_REFRESH_TOKEN = "refresh_token"
+# Configuration entry
+CONF_CONFIG_ENTRY = "config_entry"
+
+# Service constants
+CONF_PROGRAM_ID = "program_id"
+CONF_ENTITY_ID = "entity_id"
